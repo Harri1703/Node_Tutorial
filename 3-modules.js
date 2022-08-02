abfb898d035c4051731a4318c5713760eea2b796
Harri1703 <sriharri2003a@gmail.com>
2022-08-02 23:06:08 +0530

    3rd commit

--- 3-modules.js
+++ 3-modules.js
@@ -2,7 +2,9 @@ const names = require("./4-names");
 const sayHi = require("./5-utils");
 const data = require("./6-alternative-flavour");
 require("./7-mind-grenade");
-// console.log(data);
-// console.log(names);
+console.log(data);
+// // console.log(names);
+// // sayHi(names.jhon);
+// // sayHi(names.shiva);
+
 // sayHi(names.jhon);
-// sayHi(names.shiva);
